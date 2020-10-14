97462557d0deb61045e54e194977715c0b3bea6d
yejeen <yejeen@gmail.com>
2020-10-14 20:55:12 +1100

    changed to generate a test data vector once

--- tests/symbol_table_performance_test.cpp
+++ tests/symbol_table_performance_test.cpp
@@ -23,7 +23,7 @@ void printDuration(std::chrono::system_clock::time_point startTime,
 std::vector<std::string> getRandomStrings(){
     int minStringLength = 6;
     int maxStringLength = 20;
-    std::string filePath = "randomstr.txt";
+    std::string filePath = "randomStrings.txt";
 
     srand((unsigned int)time(NULL));
 
@@ -43,83 +43,78 @@ std::vector<std::string> getRandomStrings(){
     return randomStrings;
 }
 
-void insert() {
-    std::vector<std::string> randomStrings = getRandomStrings();
+void insert(std::vector<std::string> *randomStrings) {
 
     souffle::SymbolTable table;
     //start
     std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
-    for(std::vector<std::string>::size_type i = 0; i < randomStrings.size(); i++) {
-        table.lookup(randomStrings[i]);
+    for(std::vector<std::string>::size_type i = 0; i < randomStrings->size(); i++) {
+        table.lookup(randomStrings->at(i));
     }
     //end
     std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
     printDuration(startTime, endTime, "insert");
 }
 
-void insertInParallel(int numOfThreads){
-    std::vector<std::string> randomStrings = getRandomStrings();
+void insertInParallel(int numOfThreads, std::vector<std::string> *randomStrings){
 
     souffle::SymbolTable table;
     //start
     std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
 #pragma omp parallel for num_threads(numOfThreads)
-    for(std::vector<std::string>::size_type i = 0; i < randomStrings.size(); i++) {
-        table.lookup(randomStrings[i]);
+    for(std::vector<std::string>::size_type i = 0; i < randomStrings->size(); i++) {
+        table.lookup(randomStrings->at(i));
     }
     //end
     std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
     printDuration(startTime, endTime, "parallelInsert");
 }
 
-void lookup() {
-    std::vector<std::string> randomStrings = getRandomStrings();
+void lookup(std::vector<std::string> *randomStrings) {
 
     souffle::SymbolTable table;
     //input new strings
-    for(std::vector<std::string>::size_type i = 0; i < randomStrings.size(); i++) {
-        table.lookup(randomStrings[i]);
+    for(std::vector<std::string>::size_type i = 0; i < randomStrings->size(); i++) {
+        table.lookup(randomStrings->at(i));
     }
 
     //start
     std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
-    for(std::vector<std::string>::size_type i = 0; i < randomStrings.size(); i++) {
-        table.lookup(randomStrings[i]);
+    for(std::vector<std::string>::size_type i = 0; i < randomStrings->size(); i++) {
+        table.lookup(randomStrings->at(i));
     }
     //end
     std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
     printDuration(startTime, endTime, "lookup");
 }
 
-void lookupInParallel(int numOfThreads) {
-    std::vector<std::string> randomStrings = getRandomStrings();
+void lookupInParallel(int numOfThreads, std::vector<std::string> *randomStrings) {
 
     souffle::SymbolTable table;
     //input new strings
-    for(std::vector<std::string>::size_type i = 0; i < randomStrings.size(); i++) {
-        table.lookup(randomStrings[i]);
+    for(std::vector<std::string>::size_type i = 0; i < randomStrings->size(); i++) {
+        table.lookup(randomStrings->at(i));
     }
 
     //start
     std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
 #pragma omp parallel for num_threads(numOfThreads)
-    for(std::vector<std::string>::size_type i = 0; i < randomStrings.size(); i++) {
-        table.lookup(randomStrings[i]);
+    for(std::vector<std::string>::size_type i = 0; i < randomStrings->size(); i++) {
+        table.lookup(randomStrings->at(i));
     }
     //end
     std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
     printDuration(startTime, endTime, "lookupParallel");
 }
 
-void resolve() {
-    std::vector<std::string> randomStrings = getRandomStrings();
+void resolve(std::vector<std::string> *randomStrings) {
 
     souffle::SymbolTable table;
     std::vector<size_t> indices;
 
     //input new strings
-    for(std::vector<std::string>::size_type i = 0; i < randomStrings.size(); i++) {
-        indices.push_back(table.lookup(randomStrings[i]));
+    for(std::vector<std::string>::size_type i = 0; i < randomStrings->size(); i++) {
+        indices.push_back(table.lookup(randomStrings->at(i)));
     }
 
     //start
@@ -132,15 +127,14 @@ void resolve() {
     printDuration(startTime, endTime, "resolve");
 }
 
-void resolveInParallel(int numOfThreads) {
-    std::vector<std::string> randomStrings = getRandomStrings();
+void resolveInParallel(int numOfThreads, std::vector<std::string> *randomStrings) {
 
     souffle::SymbolTable table;
     std::vector<size_t> indices;
 
     //input new strings
-    for(std::vector<std::string>::size_type i = 0; i < randomStrings.size(); i++) {
-        indices.push_back(table.lookup(randomStrings[i]));
+    for(std::vector<std::string>::size_type i = 0; i < randomStrings->size(); i++) {
+        indices.push_back(table.lookup(randomStrings->at(i)));
     }
 
     //start
@@ -159,15 +153,17 @@ int main(int argc, char** argv) {
     if (argc > 1) {
         numOfThreads = std::stoi(argv[1]);
     }
-
     std::cout << "numOfThreads: " + std::to_string(numOfThreads) << std::endl;
 
-    insert();
-    insertInParallel(numOfThreads);
-    lookup();
-    lookupInParallel(numOfThreads);
-    resolve();
-    resolveInParallel(numOfThreads);
+    std::vector<std::string> randomStrings = getRandomStrings();
+    std::cout << "numOfStrings: " + std::to_string(randomStrings.size()) << std::endl;
+
+    insert(&randomStrings);
+    insertInParallel(numOfThreads, &randomStrings);
+    lookup(&randomStrings);
+    lookupInParallel(numOfThreads, &randomStrings);
+    resolve(&randomStrings);
+    resolveInParallel(numOfThreads, &randomStrings);
 }
 
 void printDuration(std::chrono::system_clock::time_point startTime,
